4007e101653cce1c8a964f67f42d497eac2ad96d
Jabin Bastian <this@jabin.id.au>
2020-05-03 00:13:57 +1000

    Add live update script to layer in another containers scripts

--- live-update.sh
+++ live-update.sh
@@ -1,5 +1,13 @@
-#!/bin/bash
+#!/bin/bash -e
 
-set -e
+owner="zuluhotelaustralia"
+repo="zha"
+image="server"
+branch="${1:-latest}"
+container="polserver"
 
-wget -qO- $1 | tar zxvf -
+remote="docker.pkg.github.com/${owner}/${repo}/${image}:${branch}"
+
+
+docker pull $remote
+docker run --rm -i $remote cat ecl-archive.tgz | docker exec -i $container tar zxvf -
