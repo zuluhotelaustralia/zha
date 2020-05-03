1b02c00a2d592e2d6f82a089042536594a38f430
Jabin Bastian <this@jabin.id.au>
2020-05-03 13:32:28 +1000

    - Remove build ecl artifact in favour of using docker image and tar pipes - Replace console scripts with ones from ModenDistro - Make live-update.sh send the script console reload sequence via stdin to POL

--- live-update.sh
+++ live-update.sh
@@ -5,9 +5,16 @@ repo="zha"
 image="server"
 branch="${1:-latest}"
 container="polserver"
-
 remote="docker.pkg.github.com/${owner}/${repo}/${image}:${branch}"
 
-
+echo "Live updating scripts via docker"
+# Pull the remote branch docker image to extract the files from
 docker pull $remote
-docker run --rm -i $remote cat ecl-archive.tgz | docker exec -i $container tar zxvf -
+
+# tar the ecl's and pipe them across to the currently running container
+docker run --rm -i $remote bash -c 'tar -czf - $(find . -type f -name "*\.ecl")' | docker exec -i $container bash -c 'tar zxf -'
+
+# Unload the scripts
+# send unlock, unload (Su) sequence twice in-case the console was already unlocked
+echo "SuSu" | socat EXEC:"docker attach $container",pty STDIN
+echo "Scripts updated and reloaded. Don't forget to merge your fixes upstream!"
